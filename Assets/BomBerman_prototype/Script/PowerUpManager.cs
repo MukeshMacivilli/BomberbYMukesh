3da553b8853836276f9eb8b0069e33b5bf1ab756
MukeshMacivilli <mukeshpt1990@gmail.com>
2018-02-26 22:41:46 +0530

    Latest work
    Working on power ups

---
+++ Assets/BomBerman_prototype/Script/PowerUpManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour 
+{
+	public int BombCount;
+	public GameObject _goSpeedUp;
+	public static PowerUpManager Instance;
+
+
+	void Awake()
+	{
+		if(Instance!=null)
+		Instance = this;
+	}
+
+	void Start () 
+	{
+		GeneratePowerup ();
+	}
+	
+
+	void Update () {
+		
+	}
+
+	public virtual void ChangeSpeed()
+	{
+		
+	}
+	public virtual void UpgaredBombCount()
+	{
+		
+	}
+	public virtual void LongBlastingCount()
+	{
+		
+	}
+	public static  bool Spawn;
+	public static bool mbRegenarate;
+	public void GeneratePowerup()
+	{
+		int x = Random.Range (1, 9);
+		int z = Random.Range (1, 8);
+
+		do {
+			{
+				GameObject obj = Instantiate (_goSpeedUp,
+					               new Vector3 (x, _goSpeedUp.transform.position.y, z), _goSpeedUp.transform.rotation);
+				//Spawn = true;
+			}
+			
+		} while(mbRegenarate);
+
+	}
+}
---
+++ Assets/BomBerman_prototype/Script/ReplaceMyPos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceMyPos : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider col)
+	{
+		print ("deleting");
+		//PowerUpManager.Instance.Spawn = false;
+		PowerUpManager.mbRegenarate = false;
+		Destroy (gameObject);
+	}
+
+	void CheckDelay(GameObject obj)
+	{
+		
+	}
+}
